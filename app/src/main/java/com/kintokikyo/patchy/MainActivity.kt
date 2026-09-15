b4fa422feaa02a29bff598166d94c6792390e188
Kintokikyo <abiksluke@gmail.com>
2026-09-16 01:21:51 +0700

    Update MainActivity.kt

--- app/src/main/java/com/kintokikyo/patchy/MainActivity.kt
+++ app/src/main/java/com/kintokikyo/patchy/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -325,38 +324,15 @@ class MainActivity : Activity() {
     // ==============================================
 
     private fun hideSystemBars() {
-
-        if (
-            Build.VERSION.SDK_INT >=
-            Build.VERSION_CODES.R
-        ) {
-
-            window.setDecorFitsSystemWindows(false)
-
-            window.insetsController?.let { controller ->
-
-                controller.hide(
-                    android.view.WindowInsets.Type.statusBars() or
-                        android.view.WindowInsets.Type.navigationBars()
-                )
-
-                controller.systemBarsBehavior =
-                    android.view.WindowInsetsController
-                        .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-
-        } else {
-
-            @Suppress("DEPRECATION")
-
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        }
+        
+        @Suppress("DEPRECATION") 
+        window.decorView.systemUiVisibility = 
+        View.SYSTEM_UI_FLAG_FULLSCREEN or 
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or 
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or 
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or 
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or 
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 
     // ==============================================
