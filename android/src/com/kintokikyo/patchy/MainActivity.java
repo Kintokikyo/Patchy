cb76b8f0c3254dc4aa6a45e65d28f5743efd1b7a
Kintokikyo <abiksluke@gmail.com>
2026-09-19 22:08:41 +0700

    Update MainActivity.java

--- android/src/com/kintokikyo/patchy/MainActivity.java
+++ android/src/com/kintokikyo/patchy/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import org.qtproject.qt.android.bindings.QtActivity;
 
 public class MainActivity extends QtActivity {
+
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
         android.util.Log.e(
@@ -19,18 +20,23 @@ public class MainActivity extends QtActivity {
     public void startActivityForResult(
             Intent intent,
             int requestCode) {
+
         android.util.Log.e(
-            "PATCHY_MAIN",
-            "CUSTOM startActivityForResult CALLED: " + intent.getAction());
+                "PATCHY_MAIN",
+                "CUSTOM startActivityForResult CALLED: "
+                + intent.getAction());
 
         if (Intent.ACTION_CREATE_DOCUMENT.equals(intent.getAction())) {
+
             intent.addFlags(
                     Intent.FLAG_GRANT_READ_URI_PERMISSION
                     | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
                     | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+
             android.util.Log.e(
-                "PATCHY_MAIN",
-                "CREATE_DOCUMENT flags = " + intent.getFlags());
+                    "PATCHY_MAIN",
+                    "CREATE_DOCUMENT flags = "
+                    + intent.getFlags());
         }
 
         super.startActivityForResult(
@@ -43,44 +49,47 @@ public class MainActivity extends QtActivity {
             int requestCode,
             int resultCode,
             Intent data) {
-        
-        android.util.Log.e(
-            "PATCHY_MAIN",
-            "onActivityResult requestCode=" + requestCode
-            + " resultCode=" + resultCode
-            + " data=" + (data != null ? data.getData() : null));
 
+        android.util.Log.e(
+                "PATCHY_MAIN",
+                "onActivityResult requestCode=" + requestCode
+                + " resultCode=" + resultCode
+                + " data=" + (data != null ? data.getData() : null));
+
+        /*
+         * Ambil persistable URI permission SEBELUM
+         * hasil diberikan ke Qt melalui super.onActivityResult().
+         */
+        if (resultCode == RESULT_OK && data != null) {
+
+            final Uri uri = data.getData();
+
+            if (uri != null) {
+
+                final int takeFlags =
+                        data.getFlags()
+                        & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                if (takeFlags != 0) {
+                    try {
+                        getContentResolver().takePersistableUriPermission(
+                                uri,
+                                takeFlags);
+                    } catch (SecurityException ignored) {
+                        // Some providers do not offer persistable permissions.
+                        // The temporary URI grant can still remain valid.
+                    }
+                }
+            }
+        }
+
+        /*
+         * Qt tetap harus menerima hasil Activity.
+         */
         super.onActivityResult(
                 requestCode,
                 resultCode,
                 data);
-
-        if (resultCode != RESULT_OK || data == null) {
-            return;
-        }
-
-        final Uri uri = data.getData();
-
-        if (uri == null) {
-            return;
-        }
-
-        final int takeFlags =
-                data.getFlags()
-                & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-
-        if (takeFlags == 0) {
-            return;
-        }
-
-        try {
-            getContentResolver().takePersistableUriPermission(
-                    uri,
-                    takeFlags);
-        } catch (SecurityException ignored) {
-            // Some providers do not offer persistable permissions.
-            // The temporary URI grant can still remain valid.
-        }
     }
 }
