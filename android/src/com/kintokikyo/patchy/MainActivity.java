a7db840d573ad90c637dcf91ee439efb7468fbb7
Kintokikyo <abiksluke@gmail.com>
2026-09-19 18:27:33 +0700

    Update MainActivity.java

--- android/src/com/kintokikyo/patchy/MainActivity.java
+++ android/src/com/kintokikyo/patchy/MainActivity.java
@@ -1,6 +1,49 @@
 package com.kintokikyo.patchy;
 
+import android.content.Intent;
+import android.net.Uri;
+
 import org.qtproject.qt.android.bindings.QtActivity;
 
 public class MainActivity extends QtActivity {
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        super.onActivityResult(
+                requestCode,
+                resultCode,
+                data);
+
+        if (resultCode != RESULT_OK || data == null) {
+            return;
+        }
+
+        final Uri uri = data.getData();
+
+        if (uri == null) {
+            return;
+        }
+
+        final int takeFlags =
+                data.getFlags()
+                & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        if (takeFlags == 0) {
+            return;
+        }
+
+        try {
+            getContentResolver().takePersistableUriPermission(
+                    uri,
+                    takeFlags);
+        } catch (SecurityException ignored) {
+            // Some providers do not offer persistable permissions.
+            // The temporary URI grant can still remain valid.
+        }
+    }
 }
