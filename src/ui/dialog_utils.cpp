16010c3e5cf19c22a91135b48b8f905f8a4d5283
Kintokikyo <abiksluke@gmail.com>
2026-09-21 14:27:32 +0700

    Dialog Utils cpp

--- src/ui/dialog_utils.cpp
+++ src/ui/dialog_utils.cpp
@@ -1744,6 +1744,9 @@ QString get_open_file_name(QWidget* parent, const QString& caption, const QStrin
 #else
   QFileDialog dialog(parent, caption, QString(), filter);
   configure_file_dialog(dialog, object_name, dir, QFileDialog::AcceptOpen, QFileDialog::ExistingFile, selected_filter);
+  #ifdef Q_OS_ANDROID
+    dialog.setMimeTypeFilters({QStringLiteral("application/octet-stream")});
+  #endif
   if (filter_details == FilterNameDetails::Hidden) {
     dialog.setOption(QFileDialog::HideNameFilterDetails, true);
   }
@@ -1768,6 +1771,9 @@ QStringList get_open_file_names(QWidget* parent, const QString& caption, const Q
 #else
   QFileDialog dialog(parent, caption, QString(), filter);
   configure_file_dialog(dialog, object_name, dir, QFileDialog::AcceptOpen, QFileDialog::ExistingFiles, selected_filter);
+  #ifdef Q_OS_ANDROID
+    dialog.setMimeTypeFilters({QStringLiteral("application/octet-stream")});
+  #endif
   if (filter_details == FilterNameDetails::Hidden) {
     dialog.setOption(QFileDialog::HideNameFilterDetails, true);
   }
