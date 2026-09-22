b08ed01d226443da0d2dc2b4a5010ce1e347c56b
Kintokikyo <abiksluke@gmail.com>
2026-09-22 16:45:58 +0700

    Update main_window_actions_menus.cpp

--- src/ui/main_window_actions_menus.cpp
+++ src/ui/main_window_actions_menus.cpp
@@ -270,7 +270,7 @@ void bind_translated_status_tip(QObject* object, const char* source,
 void MainWindow::build_menu_bar_actions(ActionBuildContext& ctx) {
   #ifdef Q_OS_ANDROID
     menuBar()->setNativeMenuBar(false);
-    //menuBar()->setVisible(true);
+    menuBar()->setVisible(true);
   #endif
   auto* file_menu = menuBar()->addMenu(tr("&File"));
   auto* edit_menu = menuBar()->addMenu(tr("&Edit"));
